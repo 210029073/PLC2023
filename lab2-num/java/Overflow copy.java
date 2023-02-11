3a05ec1d692db33624a61c8358fdf4c2f85a48c7
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-11 18:21:07 +0000

    Completed upto Question 1-5

--- lab2-num/java/Overflow copy.java
+++ lab2-num/java/Overflow copy.java
@@ -86,7 +86,7 @@ public class Overflow
         for ( int j = 1; j <= m; j++ )
         {
             ns = ns * n;
-            assert ns <= Float.MAX_VALUE : "Floating-point overflow";
+//             assert ?? : "Floating-point overflow";
             resultList.add(ns);
         }
         return resultList;
@@ -112,7 +112,7 @@ public class Overflow
         for ( int j = 0; j <= m; j++ )
         {
             ns_inv = ns_inv / n; // update from 1/n^(i-1) to 1/n^i
-            assert ns_inv >= Float.MIN_VALUE : "Floating point underflow";
+//             assert ?? : "Floating point underflow";
             geom_sum = ns_inv + geom_sum;
             resultList.add(geom_sum);
         }
--- lab2-num/lisp/exp1.lisp
+++ lab2-num/lisp/exp1.lisp
@@ -13,6 +13,8 @@
                 b)))
     )
 )
+; this is how it should look like:
+;(1 + (a + b) * (a - 1 / b))
 
 ; define function get-number
 ; in Java it would be: static float getNumber(String varName){ ...; return ...; }
